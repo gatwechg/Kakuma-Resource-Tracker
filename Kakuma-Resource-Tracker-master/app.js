bb2267e584cd20455176d077eb62528ffc6873da
Gatwech Gatmuon Mut <gatwechgatmuonmut@gmail.com>
2025-04-14 11:46:12 +0300

    chore:implement the state management

--- Kakuma-Resource-Tracker-master/app.js
+++ Kakuma-Resource-Tracker-master/app.js
@@ -4,4 +4,7 @@ const resourceList = document.getElementById("resource-list");
 const counter=document.getElementById("counter");
 const searchInput = document.querySelector(".search-input");
 const filterButton = document.querySelectorAll(".filter-btn");
-// this section is for the filter button//
+// this section demonstrates how to handle state management in javaScript//
+let resources =JSON.parse(localStorage.getItem("resources"));
+let currentFilter = "All";
+let searchTerm = "";
