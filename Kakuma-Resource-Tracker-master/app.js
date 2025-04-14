f423209f410c30d9cc2764970238cb7646978238
Gatwech Gatmuon Mut <gatwechgatmuonmut@gmail.com>
2025-04-14 11:34:16 +0300

    Chore: Grabed DOM Elements for manipulation

---
+++ Kakuma-Resource-Tracker-master/app.js
@@ -0,0 +1,7 @@
+//thi section demonstrate how to grap DOM Elements for manipulation//
+const form = document.getElementById("resource-form");
+const resourceList = document.getElementById("resource-list");
+const counter=document.getElementById("counter");
+const searchInput = document.querySelector(".search-input");
+const filterButton = document.querySelectorAll(".filter-btn");
+// this section is for the filter button//
